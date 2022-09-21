0546b8dcc2e7ce56d1c9f3bca92b834772c80f7f
euni1004 <kongeuni1004@gmail.com>
2022-09-21 11:31:23 +0900

    주석

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def home():
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-    #이거 맞아?
+
 
 
 @app.route('/login')
